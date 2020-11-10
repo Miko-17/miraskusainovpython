9acde97b70d99544b4d0824098a766399efaff64
Miko-17 <73647578+Miko-17@users.noreply.github.com>
2020-11-10 22:08:32 +0400

    Add files via upload

---
+++ HomeWork 4.py
@@ -0,0 +1,8 @@
+# ”пражнение 1
+# print('”пражнение 1. ¬ведите глубину Єлки:')
+#stars = int(input())
+print('”пражнение 1. ¬ведите глубину Єлки:')
+stars = int(input())
+
+for star in range(1, stars+1):
+print(star * '*')
