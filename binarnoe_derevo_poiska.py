1f1d5e0bedeae7dcb67e244776cc070b5cea5ee4
Miko-17 <73647578+Miko-17@users.noreply.github.com>
2020-12-05 01:39:17 +0600

    Add files via upload

---
+++ binarnoe_derevo_poiska.py
@@ -0,0 +1,54 @@
+class node:
+	def __init__(self,value=None):
+		self.value=value
+		self.left_child=None
+		self.right_child=None
+
+		
+class binary_search_tree:
+	def __init__(self):
+		self.root=None
+
+	def insert(self,value):
+		if self.root==None:
+			self.root=node(value)
+		else:
+			self._insert(value,self.root)
+
+	def _insert(self,value,cur_node):
+		if value<cur_node.value:
+			if cur_node.left_child==None:
+				cur_node.left_child=node(value)
+			else:
+				self._insert(value,cur_node.left_child)
+		elif value>cur_node.value:
+			if cur_node.right_child==None:
+				cur_node.right_child=node(value)
+			else:
+				self._insert(value,cur_node.right_child)
+		else:
+			print ("Value already in tree!")
+
+	def print_tree(self):
+		if self.root!=None:
+			self._print_tree(self.root)
+
+	def _print_tree(self,cur_node):
+		if cur_node!=None:
+			self._print_tree(cur_node.left_child)
+			print (str(cur_node.value))
+			self._print_tree(cur_node.right_child)
+
+			
+def fill_tree(tree, num = 100, max = 1000):
+     from random import randint
+     arr = [33,67,13,24,78,67,109,87,16,5,72,34]
+     for i in range(len(arr)):
+         elem = randint(0,max)
+         tree.insert(arr[i])
+     return tree
+tree = binary_search_tree()
+tree = fill_tree(tree)
+tree.print_tree()
+
+
